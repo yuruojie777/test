9fffe9f6bc2962b7eabc72cda17856eb89c9db14
61432 <roger.yu@nutpie.com.au>
2024-04-06 22:17:22 +1100

    update icon and text

--- src/Root.js
+++ src/Root.js
@@ -1,4 +1,4 @@
-import {createBrowserRouter, Router, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 import React from 'react';
 import App from "./App";
